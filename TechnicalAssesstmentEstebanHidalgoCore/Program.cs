a5937dbb7b1aa1dc35c640bc336154d42754fe9a
Esteban Hidalgo <hidalgo.esteban2594@gmail.com>
2023-04-25 12:29:22 -0400

    creating logic to separate the string into an array

--- TechnicalAssesstmentEstebanHidalgoCore/Program.cs
+++ TechnicalAssesstmentEstebanHidalgoCore/Program.cs
@@ -12,6 +12,38 @@ namespace TechnicalAssestmentEstebanHidalgo
     {
         static void Main(string[] args)
         {
+            //Console.Write("Please enter the path of the file, use the following format 'C:\\\\...:' ");
+            string pathOfFile = Console.ReadLine();
+            string stringOfNames = readDataFromFile("C:\\unsorted-names-list.txt");
+            string[] listOfNames = convertStringToArray(stringOfNames);
+
+
+            Console.ReadLine();
+        }
+
+        public static string readDataFromFile(string filePath)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                return sr.ReadToEnd();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public static string[] convertStringToArray(string str)
+        {
+
+            string[] namesArray = str.Split('\n');
+            return namesArray;
+
+
         }
 
 
