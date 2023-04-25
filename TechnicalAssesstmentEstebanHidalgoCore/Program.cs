144ec9880e39d962afff6e99ba323ca3a05b6e7b
Esteban Hidalgo <hidalgo.esteban2594@gmail.com>
2023-04-25 12:27:08 -0400

    first commit

--- TechnicalAssesstmentEstebanHidalgoCore/Program.cs
+++ TechnicalAssesstmentEstebanHidalgoCore/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalAssestmentEstebanHidalgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+    }
+}
